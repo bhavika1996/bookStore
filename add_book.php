997deb05d5a9c6445dc76a6f65b6bedbbf7afb25
bhavikapatel1996 <bhavika.patel8596@gmail.com>
2021-03-03 03:18:19 -0500

    registration

---
+++ add_book.php
@@ -0,0 +1,129 @@
+<?php
+
+require('mysqli_connect.php');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+$title = '';
+$type= '';
+$price= '';
+$year='';
+$pages='';
+$quantity='';
+
+if(empty($_POST['title'])){
+    $errorArray[] = "Title of the book is mandatory";
+    $errors = true;
+}else{
+    $title = mysqli_real_escape_string($dbc,$_POST['title']);
+}
+
+if(empty($_POST['type'])){
+    $errorArray[] = "Type of book is mandatory";
+    $errors = true;
+}else{
+    $type = mysqli_real_escape_string($dbc,$_POST['type']);
+}
+
+if(empty($_POST['price'])){
+    $errorArray[] = "Price is mandatory";
+    $errors = true;
+}else{
+
+    $price = mysqli_real_escape_string($dbc,$_POST['price']);
+}
+
+if(empty($_POST['year'])){
+    $errorArray[] = "Year of publishing is mandatory";
+    $errors = true;
+}else{
+
+    $year = mysqli_real_escape_string($dbc,$_POST['year']);
+}
+if(empty($_POST['pages'])){
+    $errorArray[] = "Pages is mandatory";
+    $errors = true;
+}else{
+
+    $pages = mysqli_real_escape_string($dbc,$_POST['pages']);
+}
+if(empty($_POST['quantity'])){
+    $errorArray[] = "Quantity is mandatory";
+    $errors = true;
+}else{
+
+    $quantity = mysqli_real_escape_string($dbc,$_POST['quantity']);
+}
+
+// echo $username.$email.$phone;
+if($errors = false && count($errorArray)>2){
+    echo "All fields mandatory";
+}{
+    $query = "INSERT INTO `bookstoredata`.`bookstorecreator`(title,book_type,price,publishing_year,pages,quantity_available) values('{$title}','{$type}','{$price}','{$year}','{$pages}','{$quantity}')";
+    if(mysqli_query($dbc,$query)){
+        header("Location: index1.php");
+    }else{
+        echo "SQL: ".$query . "Error:". mysqli_error($dbc);
+    }
+    mysqli_close($dbc);
+}
+}
+?>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Add Book</h2>
+  <form class="form-horizontal" action="add_book.php" method="post">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="title">Title:</label>
+      <div class="col-sm-10">
+        <input type="title" class="form-control" id="title" placeholder="Enter title" name="title">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="type">Type of Book:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="type" placeholder="Frictional,Motivational" name="type">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="price">Price:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="price" placeholder="Enter price" name="price">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="year">Year Of Publishing:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="year" placeholder="XXXX-XX-XX" name="year">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pages">Pages:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="pages" placeholder="Pages" name="pages">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="quantity">Quantity:</label>
+      <div class="col-sm-10">          
+        <input type="text" class="form-control" id="quantity" placeholder="Enter Quantity" name="quantity">
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" class="btn btn-default">Submit</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,37 +1,55 @@
-<?php 
-
-require("mysqli_connect.php");
-?>
-
 <html>
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
 <body>
-<span><a href="index.php">HOME &nbsp</a></span>
+<span><a href="login.php">HOME &nbsp</a>
+<span><a href="add_book.php">Add Book &nbsp</a>
+<!-- <span><a href="login.php">Logout &nbsp</a></span> -->
 <div>
-<table>
-<tr>
-    <th>BookId</th>
-    <th>Title</th>
-    <th>Book Type</th>
-    <th>Price</th>
-    <th>Publishing Year</th>
-    <th>Number of Pages</th>
-    <th>Quantity Available</th>
-</tr>
-<!-- <?php
-$query = "SELECT * FROM users";
-$result = @mysqli_query($dbc, $query);
+<table border=1>
+    <tr>
+        <th>BookId</th>
+        <th>Title</th>
+        <!-- <th>Author</th> -->
+        <th>Book Type</th>
+        <th>Price</th>
+        <th>Publishing Year</th>
+        <th>Number of Pages</th>
+        <th>Quantity Available</th>
+    </tr>
+    <?php
+    require("mysqli_connect.php");  
+    $query = "SELECT * FROM bookstorecreator";
+   // $query = "SELECT * FROM bookstorecreator bs LEFT JOIN author a ON bs.book_id = a.book_id";
+    $result = @mysqli_query($dbc, $query) or die(mysqli_error($dbc));
 
-while($rows = mysqli_fetch_array($result)){
-    $imgPath = 'uploads/' .  $rows['username'] . '/' . $rows['image'];
-    $img = '<img width="100px" height="100px" src="' . $imgPath . '">';
-    echo "<tr>";
-    echo "<td>{$rows['username']} </td>";
-    echo "<td>{$rows['profile']} </td>";
-    echo '<td>'.  $img . ' </td>';
-    echo "</tr>";
-}
-
-?> -->
+        while($rows = mysqli_fetch_array($result)){
+        // $imgPath = 'uploads/' .  $rows['username'] . '/' . $rows['image'];
+        // $img = '<img width="100px" height="100px" src="' . $imgPath . '">';
+            echo "<tr>";
+            echo "<td>{$rows['book_id']} </td>";
+            echo "<td>{$rows['title']} </td>";
+            //echo "<td>{$rows['author_name']}</td>";
+            echo "<td>{$rows['book_type']} </td>";
+            echo "<td>{$rows['price']} </td>";
+            echo "<td>{$rows['publishing_year']} </td>";
+            echo "<td>{$rows['pages']} </td>";
+            echo "<td>{$rows['quantity_available']} </td>";
+            // echo '<td>'.  $img . ' </td>';
+            echo "</tr>";
+  }
+?>
 </table>
+
+
+
+</div>
 </body>
 </html>
